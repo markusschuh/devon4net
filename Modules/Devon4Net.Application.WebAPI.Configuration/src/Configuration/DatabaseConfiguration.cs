288aaf41d1d4d93e2bab388b3be1c715f9818851
José Manuel Sánchez <jmsanmu@gmail.com>
2019-10-18 15:05:00 +0200

    Retry pattern for some providers

--- Modules/Devon4Net.Application.WebAPI.Configuration/src/Configuration/DatabaseConfiguration.cs
+++ Modules/Devon4Net.Application.WebAPI.Configuration/src/Configuration/DatabaseConfiguration.cs
@@ -13,7 +13,10 @@ namespace Devon4Net.Application.WebAPI.Configuration.Configuration
 {
     public static class DatabaseConfiguration
     {
-        public static void ConfigureDatabase<T,I>(this IServiceCollection services, ref IConfiguration configuration, string conectionStringName, DatabaseType databaseType, CosmosConfigurationParams cosmosConfigurationParams = null) where T : DbContext 
+        private const int MaxRetryDelay = 30;
+        private const int MaxRetryCount = 10;
+
+        public static void ConfigureDatabase<T>(this IServiceCollection services, ref IConfiguration configuration, string conectionStringName, DatabaseType databaseType, CosmosConfigurationParams cosmosConfigurationParams = null) where T : DbContext 
         {
             var applicationConnectionStrings = configuration.GetSection("ConnectionStrings").GetChildren();
             if (applicationConnectionStrings == null) throw new ArgumentException("There are no connection strings provided.");
@@ -25,14 +28,26 @@ namespace Devon4Net.Application.WebAPI.Configuration.Configuration
                  switch (databaseType)
                  {
                      case DatabaseType.SqlServer:
-                         options.UseSqlServer(connectionString.Value);
+                         options.UseSqlServer(connectionString.Value, sqlServerOptionsAction: sqlOptions =>
+                         {
+                             sqlOptions.EnableRetryOnFailure(
+                             maxRetryCount: MaxRetryCount,
+                             maxRetryDelay: TimeSpan.FromSeconds(MaxRetryDelay),
+                             errorNumbersToAdd: null);
+                         });
                          break;
                      case DatabaseType.InMemory:
                          options.UseInMemoryDatabase(connectionString.Value);
                          break;
                      case DatabaseType.MySql:
                      case DatabaseType.MariaDb:
-                         options.UseMySql(connectionString.Value);
+                         options.UseMySql(connectionString.Value, sqlOptions =>
+                         {
+                             sqlOptions.EnableRetryOnFailure(
+                             maxRetryCount: MaxRetryCount,
+                             maxRetryDelay: TimeSpan.FromSeconds(MaxRetryDelay),
+                             errorNumbersToAdd: null);
+                         });
                          break;
                      case DatabaseType.Sqlite:
                          options.UseSqlite(connectionString.Value);
@@ -42,7 +57,13 @@ namespace Devon4Net.Application.WebAPI.Configuration.Configuration
                          options.UseCosmos(cosmosConfigurationParams.Endpoint, cosmosConfigurationParams.Key, cosmosConfigurationParams.DatabaseName);
                          break;
                      case DatabaseType.PostgreSQL:
-                         options.UseNpgsql(connectionString.Value);
+                         options.UseNpgsql(connectionString.Value, sqlOptions =>
+                         {
+                             sqlOptions.EnableRetryOnFailure(
+                             maxRetryCount: MaxRetryCount,
+                             maxRetryDelay: TimeSpan.FromSeconds(MaxRetryDelay),
+                             errorCodesToAdd: null);
+                         });
                          break;
                      case DatabaseType.FireBird:
                          options.UseFirebird(connectionString.Value);
