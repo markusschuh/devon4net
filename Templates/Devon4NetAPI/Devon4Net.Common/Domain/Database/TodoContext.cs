b8f224d9fac733b6dcff4313ad19ebb09ac49755
jmsanmu <jmsanmu@gmail.com>
2019-10-21 08:18:32 +0200

    Added TODO context

---
+++ Templates/Devon4NetAPI/Devon4Net.Common/Domain/Database/TodoContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Devon4Net.Common.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Devon4Net.Common.Domain.Database
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext()
+        {
+        }
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Todos> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todos>(entity =>
+            {
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(255);
+            });
+        }
+    }
+}
---
+++ Templates/Devon4NetAPI/Devon4Net.Common/Domain/Entities/Todos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Devon4Net.Common.Domain.Entities
+{
+    public partial class Todos
+    {
+        public long Id { get; set; }
+        public string Description { get; set; }
+    }
+}
